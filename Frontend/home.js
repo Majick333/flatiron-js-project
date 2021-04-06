f34455d2186635c6a844aad6d01179d2e1eaee7a
Majick333 <rmccauleycode@gmail.com>
2021-04-06 05:46:41 -0500

    clean up comments

--- Frontend/home.js
+++ Frontend/home.js
@@ -5,5 +5,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const BASE_URL = "http://127.0.0.1:3000"
-//const MESSAGE_URL =`${BASE_URL}/messages`
-//const COMMENTS_URL =`${BASE_URL}/comments`
